ead865f3b5d483ba174fcbf22a10ee5242a3f27f
brandojesusrodriguezgomez <brandojesusrodriguezgomez@gmail.com>
2020-04-25 01:59:34 -0500

    subo ecomerce

---
+++ src/Components/Moda.svelte
@@ -0,0 +1,20 @@
+
+<script>
+import Modal from "./Modal.svelte";
+ let showModal = false;
+</script>
+
+
+
+
+
+<button  on:click="{() => showModal = true}">
+	show modal
+</button>
+
+{#if showModal}
+	<Modal on:close="{() => showModal = false}">
+	
+		<a href="https://www.merriam-webster.com/dictionary/modal">merriam-webster.com</a>
+	</Modal>
+{/if}
